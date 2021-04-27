8c6c02df843f65a813373f04053e3ee7570be5a5
Tlahui Raygoza <tlahuiltzin.raygoza@oracle.com>
2021-04-27 11:53:59 -0500

    Assert for Exceptions and BeforeEach and AfterEach Tags

--- HelloJunit/test/com/tlahui/courses/junit/GreetingsImplTest.java
+++ HelloJunit/test/com/tlahui/courses/junit/GreetingsImplTest.java
@@ -2,15 +2,45 @@ package com.tlahui.courses.junit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreetingsImplTest {
-
-	@Test
-	void greetShouldReturnAValidOutput() {
-		GreetingsImpl greet = new GreetingsImpl();
-		String output = greet.greet("Tlahui");
-		assertNotNull(output);
-		assertEquals("Hello", output);
+	private GreetingsImpl greetings;
+	
+	@BeforeEach public void setup() {
+		System.out.println("Setup");
+		greetings = new GreetingsImpl();
 	}
+	
+	@Test
+	public void greetShouldReturnAValidOutput() {
+		System.out.println("greetShouldReturnAValidOutput");
+		String output = greetings.greet("Tlahui");
+		assertNotNull(output);
+		assertEquals("Hello Tlahui", output);
+	}
+	
+	@Test
+	public void greetShouldThrowExcpecionWhen_NameIsNullLenghtZero() {	
+		System.out.println("greetShouldThrowExcpecionWhen_NameIsNullLenghtZero");
+		Assertions.assertThrows(IllegalArgumentException.class, 
+				() -> { greetings.greet(""); });		
+	}
+	
+	@Test
+	public void greetShouldThrowExcpecionWhen_LenghtZero() {	
+		System.out.println("greetShouldThrowExcpecionWhen_LenghtZero");
+		Assertions.assertThrows(IllegalArgumentException.class, 
+				() -> { greetings.greet(null); });
+	}
+	
+	@AfterEach public void tearDown() {
+		System.out.println("Tear Down");
+		greetings = null;
+		
+	}
+	
 }
