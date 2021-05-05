1d1786803464aba2bacd8bf19a2ad8c492d77780
Tlahui Raygoza <tlahuiltzin.raygoza@oracle.com>
2021-05-05 18:02:13 -0500

    BOImpl Logic added

--- OrderProcessingService/src/com/tlahui/order/bo/OrderBOImpl.java
+++ OrderProcessingService/src/com/tlahui/order/bo/OrderBOImpl.java
@@ -13,21 +13,42 @@ public class OrderBOImpl implements OrderBO {
 	@Override
 	public boolean placeOrder(Order order) throws BOException {
 		try {
-			dao.create(order);
+			int result = dao.create(order);
+			if(result == 0) {
+				return false;
+			}
 		} catch (SQLException e) {
 			throw new BOException(e);			
 		}
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean cancelOrder(int id) throws BOException {
-		return false;
+		try {
+			Order order = dao.read(id);
+			order.setStatus("cancelled");
+			int result = dao.update(order);
+			if(result == 0) {
+				return false;
+			}
+		} catch (SQLException e) {
+			throw new BOException(e);	
+		}		
+		return true;
 	}
 
 	@Override
 	public boolean deleteOrder(int id) throws BOException {
-		return false;
+		try {
+			int result = dao.delete(id);
+			if(result == 0 ) {
+				return false;
+			}
+		} catch (SQLException e) {
+			throw new BOException(e);	
+		}
+		return true;
 	}
 
 	public OrderDAO getDao() {
